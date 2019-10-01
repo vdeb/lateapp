0119ad73b2a101671d51261e4d333dd5217127b0
DEBRAY Victor <victor.debray@wavestone.com>
2019-10-01 19:25:22 +0200

    leak

--- lateapp/imports/ui/App.jsx
+++ lateapp/imports/ui/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
+  <Container fluid>
+    <h1>Welcome to LateApp!</h1>
+    <Row>
+      <Col md={3}>
+        <h4>Mes cours :</h4>
+        <ListGroup>
+          <ListGroup.Item>Innov 2019 - Semestre 1</ListGroup.Item>
+          <ListGroup.Item>Innov 2019 - Semestre 2</ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col md={9}>Fenêtre principale</Col>
+    </Row>
+  </Container>
 );
 
 export default App;
