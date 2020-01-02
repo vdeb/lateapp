9182251225d8bc99698fbc3e1652ed458058d37f
Victor Debray <victor.debray@clickandboat.com>
2020-01-02 12:50:54 +0100

    #2yk9b6 Creates student component and initialized student collection

--- lateapp/imports/startup/server/index.js
+++ lateapp/imports/startup/server/index.js
@@ -1,3 +1,5 @@
+import students from "../../api/students";
+
 // This defines a starting set of data to be loaded if the app is loaded with an empty db.
 //import './fixtures.js';
 
@@ -10,3 +12,5 @@
 // This defines all the collections, publications and methods that the application provides
 // as an API to the client.
 //import './register-api.js';
+
+import '../../api/students.js';
--- lateapp/imports/ui/App.jsx
+++ lateapp/imports/ui/App.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+import Student from './Student.jsx';
 
-const App = () => (
-  <Container fluid>
-    <h1>Welcome to LateApp!</h1>
-    <Row>
-      <Col md={3}>
-        <h4>Mes cours :</h4>
-        <ListGroup>
-          <ListGroup.Item>Innov 2019 - Semestre 1</ListGroup.Item>
-          <ListGroup.Item>Innov 2019 - Semestre 2</ListGroup.Item>
-        </ListGroup>
-      </Col>
-      <Col md={9}>Fenêtre principale</Col>
-    </Row>
-  </Container>
-);
 
-export default App;
+export default class App extends Component {
+
+  getStudents() {
+    return [
+      {_id: 1, name: "Jose Bove"},
+      {_id: 2, name: "Arthur Mori"},
+      {_id: 3, name: "Ella Bubulle"}
+    ]
+  }
+
+  renderStudents() {
+    return this.getStudents().map( (student) => (
+      <Student key={student._id} student={student} />
+    ));
+  }
+
+  render() {
+    return (
+      <Container fluid>
+      <h1>Welcome to LateApp!</h1>
+      <Row>
+        <Col md={2}>
+          <h4>Mes cours :</h4>
+          <ListGroup>
+            <ListGroup.Item>Innov 2019 - Semestre 1</ListGroup.Item>
+            <ListGroup.Item>Innov 2019 - Semestre 2</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={10}>
+          Fenêtre principale
+          <ul>{this.renderStudents()}</ul>
+        </Col>
+      </Row>
+    </Container>
+    );
+  }
+}
