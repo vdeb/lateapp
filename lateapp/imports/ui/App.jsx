7a4c328ef35acae8bbbdb580491e7aeaf4e713d3
Victor Debray <victor.debray@clickandboat.com>
2020-01-04 14:11:43 +0100

    #2yk9b6 lists all the students of the course

--- lateapp/imports/ui/App.jsx
+++ lateapp/imports/ui/App.jsx
@@ -4,25 +4,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 import { Students } from '../api/students.js';
 
-import Student from './Student.jsx';
+import SimpleStudent from './components/SimpleStudent.jsx';
 
 
 class App extends Component {
 
-  getStudents() {
-    return [
-      {_id: 1, name: "Jose Bove"},
-      {_id: 2, name: "Arthur Mori"},
-      {_id: 3, name: "Ella Bubulle"}
-    ]
-  }
-
   renderStudents() {
     return this.props.students.map( (student) => (
-      <Student key={student._id} student={student} />
+      <SimpleStudent key={student._id} student={student} />
     ));
   }
 
@@ -30,18 +23,14 @@ class App extends Component {
     return (
       <Container fluid>
       <h1>Welcome to LateApp!</h1>
-      <Row>
-        <Col md={2}>
-          <h4>Mes cours :</h4>
-          <ListGroup>
-            <ListGroup.Item>Innov 2019 - Semestre 1</ListGroup.Item>
-            <ListGroup.Item>Innov 2019 - Semestre 2</ListGroup.Item>
-          </ListGroup>
+        <Col xs={12} md={12}>
+          <Row>
+          {this.renderStudents()}
+          {this.renderStudents()}
+          {this.renderStudents()}
+          {this.renderStudents()}
+          </Row>
         </Col>
-        <Col md={10}>
-          <ul>{this.renderStudents()}</ul>
-        </Col>
-      </Row>
     </Container>
     );
   }
@@ -49,6 +38,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    students: Students.find({}).fetch(),
+    students: Students.find({}, { sort: { name: 1 } }).fetch(),
   };
 })(App);
--- lateapp/imports/ui/Student.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Student extends Component {
-    render() {
-        return (
-            <li>{this.props.student.name}</li>
-        )
-    }
-}
