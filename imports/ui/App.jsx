06b359464b047bfd4c482bfed135f15cc1f67cef
Victor Debray <victor.debray@clickandboat.com>
2020-01-05 14:59:42 +0100

    #1wkgbt adds the form space

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,8 +3,7 @@ import { withTracker } from 'meteor/react-meteor-data';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button'
 
 import { Students } from '../api/students/students.js';
 
@@ -13,19 +12,48 @@ import SimpleStudent from './components/SimpleStudent.jsx';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      FormVisible: false
+    }
+
+    this.displayForm = this.displayForm.bind(this);
+  }
+
   renderStudents() {
     return this.props.students.map( (student) => (
       <SimpleStudent key={student._id} student={student} />
     ));
   }
 
+  displayForm() {
+    this.setState({
+      FormVisible: !this.state.FormVisible
+    })
+  }
+
+  renderForm() {
+    return (
+      <p>Hellooo</p>
+    )
+  }
+
   render() {
     return (
       <Container fluid>
       <h1>Welcome to LateApp!</h1>
+      <h2>
+        Cours Innovation 2020
+        <Button
+          onClick={this.displayForm}
+          style= {{display: this.state.FormVisible && 'none'}}
+        >Commencer une nouvelle session</Button>
+      </h2>
+      {this.state.FormVisible?this.renderForm():<div></div>}
         <Col xs={12} md={12}>
           <Row>
-          {this.renderStudents()}
+          {this.props.students.length > 0 && this.renderStudents()}
           {this.renderStudents()}
           {this.renderStudents()}
           {this.renderStudents()}
